0d5183632cf2752262198989d936414a91ca3ca8
bmCDV <bmikolajczak@edu.cdv.pl>
2021-07-30 23:02:05 +0200

    entire project so far

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,179 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="ctr">
+      
+    <transition-group name="fade">
+        <home v-if="startFlag==true" @started="started"></home>
+        <questions v-if="questionsAnswered<questions.length && startFlag==false" 
+        :questions="questions"
+        :questionsAnswered="questionsAnswered"
+        @question-answered="questionAnswered"></questions>
+        
+        <result v-else-if="questionsAnswered>=questions.length && startFlag==false" :results="results" :totalCorrect="totalCorrect"></result>
+    </transition-group>
+    <button type="button" class="reset-btn" @click.prevent="reset" v-if="questionsAnswered===questions.length">Reset Quiz</button>
+</div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import Questions from './components/Questions.vue'
+import Result from './components/Result.vue'
+import Home from './components/Home.vue'
 export default {
   name: 'App',
-  components: {
-    HelloWorld
+  components:{Questions,Result,Home},
+  data(){
+    return{
+      questionsAnswered:0,
+      totalCorrect:0,
+      startFlag:true,
+      questions: [
+          {
+              q: 'What is 2 + 2?', 
+              answers: [
+                  {
+                      text: '4',
+                      is_correct: true
+                  },
+                  {
+                      text: '3',
+                      is_correct: false 
+                  },
+                  {
+                      text: '1',
+                      is_correct: false 
+                  },
+                  {
+                      text: '5',
+                      is_correct: false 
+                  }
+              ] 
+          },
+          { 
+              q: 'What is √9?', 
+              answers: [
+                  {
+                      text: '6',
+                      is_correct: false
+                  },
+                  {
+                      text: '2.333',
+                      is_correct: false 
+                  },
+                  {
+                      text: '3',
+                      is_correct: true 
+                  },
+                  {
+                      text: '3.3',
+                      is_correct: false 
+                  }
+              ] 
+          },
+          { 
+              q: 'what is 2+(3+5)*6', 
+              answers: [
+                  {
+                      text: '49',
+                      is_correct: false
+                  },
+                  {
+                      text: '50',
+                      is_correct: true 
+                  },
+                  {
+                      text: '51',
+                      is_correct: false 
+                  },
+                  {
+                      text: '48',
+                      is_correct: false
+                  }
+              ] 
+          },
+          { 
+              q: 'what is √9+√4', 
+              answers: [
+                  {
+                      text: '5',
+                      is_correct: true
+                  },
+                  {
+                      text: '6',
+                      is_correct: false 
+                  },
+                  {
+                      text: '4',
+                      is_correct: false 
+                  },
+                  {
+                      text: '9',
+                      is_correct: false
+                  }
+              ] 
+          },
+          { 
+              q: 'what is 100*12+√9', 
+              answers: [
+                  {
+                      text: '1200',
+                      is_correct: false
+                  },
+                  {
+                      text: '1202',
+                      is_correct: false 
+                  },
+                  {
+                      text: '1203',
+                      is_correct: true 
+                  },
+                  {
+                      text: '1204',
+                      is_correct: false
+                  }
+              ] 
+          },
+
+      ],
+      results: [
+          {
+              min: 0,
+              max: 2,
+              title: "Try again!",
+              desc: "Do a little more studying and you may succeed!"
+          },
+          {
+              min: 3,
+              max: 5,
+              title: "Great job!",
+              desc: "You did well but there's still room for improvement, is it?"
+          },
+          {
+              min: 5,
+              max: 5,
+              title: "You're a genius!!!",
+              desc: "Someone has been studying really hard lately!"
+          },
+
+      ]
+    }
+  },
+methods:{
+    questionAnswered(is_correct){
+      if(is_correct){
+        this.totalCorrect++;
+      }
+      this.questionsAnswered++;
+    },
+    reset(){
+        this.questionsAnswered=0;
+        this.totalCorrect=0;
+    },
+    started(){
+        this.startFlag=!this.startFlag;
+    }
   }
+
 }
+
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
---
+++ src/components/Home.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="home">
+        <div class="title">
+            <h1>Ready to start a Quiz?</h1>
+        </div>
+        <button @click="started" class="start-button">Let's begin!</button>    
+    </div>
+</template>
+
+<script>
+export default {
+    emits:['started'],
+    methods:{
+        started(){
+            this.$emit('started');
+        }
+    }
+}
+</script>
